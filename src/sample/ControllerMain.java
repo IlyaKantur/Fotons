5e44021dffc53b151b1f552fe70578c8439cd455
IlyaKantur <il.kantur@mail.ru>
2020-03-03 17:10:59 +0500

    Добавлено меню, и 2 кнопки

---
+++ src/sample/ControllerMain.java
@@ -0,0 +1,27 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ControllerMain {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button Fold;
+
+    @FXML
+    private Button Save;
+
+    @FXML
+    void initialize() {
+    }
+}
+
+
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,13 +10,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("sampleMain.fxml"));
+        primaryStage.setTitle("Foton_Detect");
+        primaryStage.setScene(new Scene(root, 740, 560));
         primaryStage.show();
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
